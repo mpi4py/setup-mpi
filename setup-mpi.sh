0f3cd3430047f6a9395b83bc7d0d2c2ccc4e640a
Lisandro Dalcin <dalcinl@gmail.com>
2023-10-07 08:16:41 +0300

    action: Support alternative MPI names
    openmpi:  open-mpi, ompi
    intelmpi: intel-mpi, impi, intel
    msmpi:    microsoft-mpi, microsoftmpi, microsoft

--- setup-mpi.sh
+++ setup-mpi.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 set -eu
 
-MPI=$(echo "${1:-}" | tr '[:upper:]' '[:lower:]')
+MPI=$(echo "${1:-}" | tr '[:upper:]' '[:lower:]' | tr -d '-')
+
+case $MPI in
+    mpich)
+        MPI=mpich
+        ;;
+    ompi | openmpi)
+        MPI=openmpi
+        ;;
+    impi | intelmpi | intel)
+        MPI=intelmpi
+        ;;
+    msmpi | microsoftmpi | microsoft)
+        MPI=msmpi
+        ;;
+esac
 
 setup-apt-intel-oneapi () {
     apt_repo_url=https://apt.repos.intel.com/
