4a16f37afe6c994a30f2e2602bec5768014e9667
Lisandro Dalcin <dalcinl@gmail.com>
2025-02-26 14:48:14 +0300

    Intel MPI: Update to release 2021.14.2

--- setup-mpi.sh
+++ setup-mpi.sh
@@ -58,6 +58,7 @@ setup-win-intel-oneapi-mpi () {
     hash=ea625b1d-8a8a-4bd5-b31d-7ed55af45994 version=2021.13.1 build=768
     hash=44400f77-51cb-4f15-8424-0e11eeb41832 version=2021.14.0 build=785
     hash=e9f49ab3-babd-4753-a155-ceeb87e36674 version=2021.14.1 build=8
+    hash=29e1f37b-f7c7-4cd6-988c-6ddf80aadf6a version=2021.14.2 build=901
     baseurl=https://registrationcenter-download.intel.com
     subpath=akdlm/IRC_NAS/$hash
     if test $version \< 2021.14.0; then
